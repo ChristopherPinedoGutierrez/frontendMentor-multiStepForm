ec80220bfed710a04095af41f5abcfe5bc485414
Christopher Pinedo <cdpg.dev@gmail.com>
2025-01-12 20:42:38 -0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container mx-auto flex min-h-screen items-center justify-center bg-slate-200">
+      <div className="rounded-lg border bg-slate-100 p-4">
+        <h1 className="bg-inherit text-4xl font-bold">
+          Form project with ReactJs, Typescript and TaiwindCss
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
